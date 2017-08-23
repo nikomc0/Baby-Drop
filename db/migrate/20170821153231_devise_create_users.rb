9984b1b90d52f12ba96ebffc9af50f6de022d85d
Dominick Del Bosque <dominick@theosfp.com>
2017-08-23 12:02:46 -0700

    Changed phone from integer to string

--- db/migrate/20170821153231_devise_create_users.rb
+++ db/migrate/20170821153231_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string  :encrypted_password, null: false, default: ""
       t.string  :first_name
       t.string  :last_name
-      t.integer :phone, :integer, limit: 10
+      t.string  :phone
       t.string  :child
       t.string  :spouse
       t.string  :facility
